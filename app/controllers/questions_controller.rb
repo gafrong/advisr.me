623cfd8ec54ca5f62206951a107ea0320ab8ff43
Ji Lee <gafrong@gmail.com>
2014-10-28 12:15:47 +1100

    first commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,29 @@
+class QuestionsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @categories = Category.all
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new
+    @question.user_id = current_user.id
+    @question.content = params[:question][:content]
+    @question.category_id = params[:question][:category_id]
+   
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  root 'pages#index'
+  get 'pages/about' => 'pages#about'
+  get 'pages/faq' => 'pages#faq'
+  
+  resources :categories
+  resources :questions
+  
+end
